afd5a9015b54a7a41ea2f5177c124994a5209c3d
= <=>
2024-10-26 12:03:15 -0300

    teste

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react'; 
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import { createRoot } from 'react-dom/client'; 
+import App from './App'; 
 import { AuthProvider } from './context/auth'; 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </StrictMode>
-);
+const rootElement = document.getElementById('root'); // Obter o elemento
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </StrictMode>
+  );
+} else {
+  console.error('Elemento "root" não encontrado. Verifique se o HTML está configurado corretamente.');
+}
